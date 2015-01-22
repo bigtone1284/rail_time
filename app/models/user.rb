3f15b5ddcf6dcb57248bbc1af54472303e50df92
bigtone1284 <anthony.defreitas@gmail.com>
2015-01-22 14:08:51 -0500

    added some validators to User Class

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,20 @@
 class User < ActiveRecord::Base
 	has_many :alerts
+	has_secure_password
+	validates_uniqueness_of :username,:email_address, :phone_number
+	validates_presence_of :username
+	validate :email_address_validator, :phone_number_validator
+
+	def email_address_validator
+		if email_address != nil && !(/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i.match(email_address))
+			errors.add(:email_address, "Not a valid Email Address.")
+		end
+	end
+
+	def phone_number_validator
+		if phone_number != nil && !(/^(\+0?1\s)?\(?\d{3}\)?[\s.-]\d{3}[\s.-]\d{4}$/).match(phone_number)
+			errors.add(:phone_number, "Not a valid US phone number.")
+		end
+	end
+
 end
--- lib/mta.rb
+++ lib/mta.rb
@@ -1,50 +1,7 @@
 module MTA
 
 	API_KEY = ENV['MTA_KEY']
-	def trains(line_number)
-	address = "https://mnorth.prod.acquia-sites.com/wse/gtfsrtwebapi/v1/gtfsrt/#{ENV["MTA_API"]}/getfeed"
-	headers = { 'Accept' => 'application/x-protobuf' }
-	trains = HTTParty.get(address, :options => {'headers' => headers})
-	trains["entity"].select do |train| 
-		if train["trip_update"]["trip"]["route_id"] == line_number
-			train
-		end		
-	end	
-end
-
-		def direction_filter(line_number, direction)
-			trains_set = trains(line_number)
-			line_number = line_number.to_i
-			if direction == 'north'
-				if line_number == 3
-					trains_set.select do |train| 
-						if train["id"].to_i.even?
-							train
-						end
-					end
-				elsif line_number == 1 || line_number == 2
-					trains_set.select do |train| 
-						if train["id"].to_i.odd?
-							train
-						end
-					end
-				end
-			elsif direction == 'south'
-				if line_number == 3
-					trains_set.select do |train| 
-						if train["id"].to_i.odd?
-							train
-						end
-					end
-				elsif line_number == 1 || line_number == 2
-					trains_set.select do |train| 
-						if train["id"].to_i.even?
-							train
-						end
-					end
-				end
-			end
-		end
+	
 
 
 end
