b88ebba91fe3c8c80af89dd2be80ef6e95268e87
bigtone1284 <anthony.defreitas@gmail.com>
2015-01-22 16:29:29 -0500

    finished mta module

--- lib/mta.rb
+++ lib/mta.rb
@@ -1,7 +1,88 @@
 module MTA
 
-	API_KEY = ENV['MTA_KEY']
-	
+	def self.trains(line_number)
+		address = "https://mnorth.prod.acquia-sites.com/wse/gtfsrtwebapi/v1/gtfsrt/#{ENV["MTA_API"]}/getfeed"
+		headers = { 'Accept' => 'application/x-protobuf' }
+		trains = HTTParty.get(address, :options => {'headers' => headers})
+		trains["entity"].select do |train| 
+			if train["trip_update"]["trip"]["route_id"] == line_number
+				train
+			end		
+		end	
+	end
 
+	def self.direction_filter(line_number, direction)
+		trains_set = trains(line_number)
+		line_number = line_number.to_i
+		if direction == 'north'
+			if line_number == 3
+				trains_set.select do |train| 
+					if train["id"].to_i.even?
+						train
+					end
+				end
+			elsif line_number == 1 || line_number == 2
+				trains_set.select do |train| 
+					if train["id"].to_i.odd?
+						train
+					end
+				end
+			end
+		elsif direction == 'south'
+			if line_number == 3
+				trains_set.select do |train| 
+					if train["id"].to_i.odd?
+						train
+					end
+				end
+			elsif line_number == 1 || line_number == 2
+				trains_set.select do |train| 
+					if train["id"].to_i.even?
+						train
+					end
+				end
+			end
+		end
+	end
+
+	def self.stop_on_train?(train, stop_id)
+		train["trip_update"]["stop_time_update"].each do |stop|
+			if stop["stop_id"] == stop_id
+				return true
+			end
+		end
+		return false
+	end
+
+	def self.trains_stop(trains, stop_id)
+		trains.select do |train|
+			if stop_on_train?(train, stop_id)
+				train
+			end
+		end
+	end
+
+	def self.find_stop(stop_listing, stop_id)
+		stop_listing.select do |stop|
+			if stop["stop_id"] == stop_id
+				stop
+			end
+		end
+	end
+
+	def self.station_info(trains, stop_id)
+		train_hash = Hash.new
+		trains.each do |train|
+			stop_info = find_stop(train["trip_update"]["stop_time_update"], stop_id)
+			train_hash[train['id']] = stop_info[0]
+		end
+		train_hash
+	end
+
+	def self.current_station_hash(line_number, direction, stop_id)
+		trains = direction_filter(line_number, direction)
+		train_filter = trains_stop(trains, stop_id)
+		station_info(train_filter, stop_id)
+	end
 
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,1 +1,36 @@
 require 'rails_helper'
+
+describe User do 
+
+	it { is_expected.to validate_presence_of :username }
+
+	it { is_expected.to validate_uniqueness_of :username }
+	it { is_expected.to validate_uniqueness_of :email_address }
+	it { is_expected.to validate_uniqueness_of :phone_number }
+
+	let(:hari) {new_user = User.create( 
+		username: "superman",
+		first_name: "hari",
+		last_name: "Doe",
+		password: "hunter2",
+		password_confirmation: "hunter2"
+	)}
+
+	let(:mck) {new_user = User.create( 
+		username: "superman",
+		first_name: "mck",
+		last_name: "Doe",
+		password: "hunter2",
+		password_confirmation: "hunter2"
+	)}
+
+	it "saves a valid user" do
+		expect(hari).to be_valid
+	end
+
+	it "does not allow for duplicate usernames" do
+		expect(mck).to_not be_valid
+	end
+
+
+end
