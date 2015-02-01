9fab266ebd1ae66d2ed563bc649a38495d0fb70f
bigtone1284 <anthony.defreitas@gmail.com>
2015-02-01 15:52:25 -0500

    merge conflict

--- app/mailers/alert_mailer.rb
+++ app/mailers/alert_mailer.rb
@@ -1,10 +1,9 @@
 class AlertMailer < ActionMailer::Base
   default from: ENV["GMAIL_MTA"]
   
-  def alert_email(user)
-  	@user = user
-  	mail(to @user.email, subject: "Metro North Alerts")
+  def alert_email(alert)
+  	@alert = alert
+  	mail(to @alert.email, subject: "Metro North Alerts")
   end
 
-export GMAIL_MTA_PASSWORD='rtm987654321'
 end
